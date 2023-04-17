debf56eb990acc45b840ee037a0ed65c63bf26d6
Tommy Miguel <tommymiguel@Tommys-MacBook-Pro.local>
2023-04-17 16:05:54 -0700

    Working on the main layout for home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './components/Login'
 import './App.css'
+import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -9,6 +11,11 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/home" element={<div>
+            <Header></Header>
+            <Home></Home>
+
+          </div>}/>
         </Routes>
       </div>
     </Router>
