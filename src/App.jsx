80ae0c1d25f1f30774df384c645a252b1f3305e7
Tommy Miguel <tommymiguel@Tommys-MacBook-Pro.local>
2023-04-16 19:16:27 -0700

    Base Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <h2>Let's build the linkeIn Clone</h2>
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
