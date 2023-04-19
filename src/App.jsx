7f5b6539d6e62c81c0fed7afbcfc00f3c9af8f3d
Tommy Miguel <tommymiguel@Tommys-MacBook-Pro.local>
2023-04-18 22:47:26 -0700

    functional user image when logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
 // import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './components/Login'
-import './App.css'
-import Header from './components/Header';
-import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import { useEffect } from "react";
+import { getUserAuth } from "./actions";
+import { connect } from "react-redux";
 
-function App() {
+function App(props) {
+  useEffect(() => {
+    props.getUserAuth();
+  }, []);
+
   return (
     <Router>
       <div className="App">
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/home" element={<div>
-            <Header></Header>
-            <Home></Home>
-
-          </div>}/>
+          <Route
+            path="/home"
+            element={
+              <div>
+                <Header></Header>
+                <Home></Home>
+              </div>
+            }
+          />
         </Routes>
       </div>
     </Router>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getUserAuth: () => dispatch(getUserAuth()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
